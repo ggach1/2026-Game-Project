39dfd6be5df89166414096f97f73a162d3852134
ggach16 <dlsdnj1234@gmail.com>
2026-09-07 11:40:31 +0900

    fixed : player settting

---
+++ Assets/00.Work/CIW/Code/System/InputSO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace CIW.Code.System
+{
+    [CreateAssetMenu(fileName = "Input", menuName = "SO/Input", order = 1)]
+    public class InputSO : ScriptableObject, Controls.IPlayerActions
+    {
+        public event Action OnInteractPressed;
+        public event Action OnJumpPressed;
+        public event Action<Vector2> OnMovePressed;
+
+        Controls _controls;
+        
+        private void OnEnable()
+        {
+            if (_controls == null)
+            {
+                _controls = new Controls();
+                _controls.Player.SetCallbacks(this);
+            }
+
+            _controls.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _controls.Player.Disable();
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                OnInteractPressed?.Invoke();
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                OnJumpPressed?.Invoke();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            Vector2 vec = context.ReadValue<Vector2>();
+            OnMovePressed?.Invoke(vec);
+        }
+    }
+}
