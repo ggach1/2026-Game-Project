62f7320d0ae3d60646dd905d416b82636a1d367e
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-09-16 22:33:10 +0900

    feat : Saw

---
+++ Assets/00.Work/KMJ/Code/01.Object/InteractSignObject.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace KMJ.Code.Object
+{
+    public class InteractSignObject : MonoBehaviour
+    {
+        [SerializeField] private LayerMask targetLayer;
+        [SerializeField] private UnityEvent OnInteractEvent;
+
+        private bool _isActive = true;
+
+        public void IsActiveSign(bool isActive)
+        {
+            _isActive = isActive;
+        }
+        
+        private void Sign()
+        {
+            if (!_isActive) return;
+            
+            OnInteractEvent?.Invoke();
+        }
+        
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if ((targetLayer.value & (1 << other.gameObject.layer)) != 0)
+            {
+                Sign();
+            }
+        }
+    }
+}
---
+++ Assets/00.Work/KMJ/Code/01.Object/Missile.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace KMJ.Code.Object
+{
+    public class Missile : MonoBehaviour
+    {
+        [SerializeField] private LayerMask whatIsGround;
+        [SerializeField] private UnityEvent onMissileTimeEvent;
+        [SerializeField] private float eventTime;
+        [SerializeField] private Saw sawPrefab;
+        
+        private void Awake()
+        {
+            StartCoroutine(EventStartTimer());
+        }
+
+        private IEnumerator EventStartTimer()
+        {
+            yield return new WaitForSeconds(eventTime);
+            
+            onMissileTimeEvent.Invoke();
+        }
+
+        private IEnumerator MissileDetectedGround()
+        {
+            yield return new WaitForSeconds(1.5f);
+            
+            Saw saw1 = Instantiate(sawPrefab, transform.position, Quaternion.identity);
+            Saw saw2 = Instantiate(sawPrefab, transform.position, Quaternion.identity);
+                
+            saw1.SetDirection(Vector2.right);
+            saw2.SetDirection(Vector2.left);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if ((whatIsGround.value & (1 << collision.gameObject.layer)) != 0)
+            {
+                StartCoroutine(MissileDetectedGround());    
+            }
+        }
+    }
+}
