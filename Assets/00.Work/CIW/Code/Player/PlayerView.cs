3756e645bd2634ad402fe58424928c06fd5ecdbb
ggach16 <dlsdnj1234@gmail.com>
2026-09-22 10:29:26 +0900

    fixed : player anim controller

--- Assets/00.Work/CIW/Code/Player/PlayerView.cs
+++ Assets/00.Work/CIW/Code/Player/PlayerView.cs
@@ -10,11 +10,19 @@ namespace CIW.Code.Player
     /// </summary>
     public class PlayerView : Module
     {
-        [SerializeField] string groundedParameter = "";
-        [SerializeField] string verticalSpeedParameter = "";
-        [SerializeField] string moveSpeedParameter = "";
-        [SerializeField] string deathTrigger = "";
-        [SerializeField] string respawnTrigger = "";
+        [SerializeField] string groundedParameter = "Grounded";
+        [SerializeField] string verticalSpeedParameter = "VertSpeed";
+        [SerializeField] string moveSpeedParameter = "MoveSpeed";
+        [SerializeField] string deathTrigger = "Death";
+        [SerializeField] string respawnTrigger = "Respawn";
+
+        [Header("Playback Speed")]
+        [SerializeField] string runPlaybackParameter = "RunPlayback";
+        [SerializeField] string airPlaybackParameter = "AirPlayback";
+        [SerializeField, Min(0f)] float minRunPlaybackSpeed = 0.75f;
+        [SerializeField, Min(0f)] float maxRunPlaybackSpeed = 1.25f;
+        [SerializeField, Min(0f)] float minAirPlaybackSpeed = 0.75f;
+        [SerializeField, Min(0f)] float maxAirPlaybackSpeed = 1.35f;
 
         CIW.Code.System.EntityAnimator _entityAnimator;
         PlayerMotor2D _motor;
@@ -25,6 +33,8 @@ namespace CIW.Code.Player
         int _moveSpeedHash;
         int _deathHash;
         int _respawnHash;
+        int _runPlaybackHash;
+        int _airPlaybackHash;
 
         public override void Initialize(ModuleOwner owner)
         {
@@ -39,6 +49,8 @@ namespace CIW.Code.Player
             _moveSpeedHash = ToHash(moveSpeedParameter);
             _deathHash = ToHash(deathTrigger);
             _respawnHash = ToHash(respawnTrigger);
+            _runPlaybackHash = ToHash(runPlaybackParameter);
+            _airPlaybackHash = ToHash(airPlaybackParameter);
         }
 
         private void LateUpdate()
@@ -49,6 +61,9 @@ namespace CIW.Code.Player
             SetMoveDirection(_motor.GetHorizontalSpeed());
             SetGrounded(_groundSensor.IsGrounded);
             SetVerticalSpeed(_motor.GetVerticalSpeed());
+            SetPlaybackSpeed(
+                _motor.GetNormalizedHorizontalSpeed(),
+                _motor.GetNormalizedVerticalSpeed());
         }
 
         public void SetMoveDirection(float dir)
@@ -71,9 +86,24 @@ namespace CIW.Code.Player
             _entityAnimator?.SetFloat(_verticalSpeedHash, spd);
         }
 
+        private void SetPlaybackSpeed(float horizontalRatio, float verticalRatio)
+        {
+            // 물리 속도 자체가 아닌 제한된 배율을 전달해 클립이 멈추거나 과속하는 것을 방지합니다.
+            float runPlayback = Mathf.Lerp(
+                minRunPlaybackSpeed, maxRunPlaybackSpeed, Mathf.Clamp01(horizontalRatio));
+            float airPlayback = Mathf.Lerp(
+                minAirPlaybackSpeed, maxAirPlaybackSpeed, Mathf.Clamp01(verticalRatio));
+
+            _entityAnimator?.SetFloat(_runPlaybackHash, runPlayback);
+            _entityAnimator?.SetFloat(_airPlaybackHash, airPlayback);
+        }
+
         public void PlayDeath(DeathContext context)
         {
             _entityAnimator?.SetTrigger(_deathHash);
+
+            // 전용 사망 애니메이션이 추가되기 전까지 즉시 숨기는 방식으로 사망 상태를 표현합니다.
+            _entityAnimator?.SetVisible(false);
         }
         public void PlayRespawn()
         {
